7883b06e4851301705f082dd47198ccddcd1bd73
Johan Halin <johan@richie.fi>
2016-12-09 15:56:48 +0200

    Removed test code and unused imports.

--- app/src/main/java/com/aerodeko/pfm/MainActivity.kt
+++ app/src/main/java/com/aerodeko/pfm/MainActivity.kt
@@ -5,8 +5,6 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.util.Log
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
 
@@ -22,9 +20,6 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-
-        // FIXME: remove this shit
-        Log.d("test", "test")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
