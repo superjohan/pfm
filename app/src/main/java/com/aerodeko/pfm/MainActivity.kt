ed523bb7a59c9bc681a7608dcc560f2e0e77b3a3
Johan Halin <johan@richie.fi>
2016-12-09 15:41:56 +0200

    Test commit for Slack integrations.

--- app/src/main/java/com/aerodeko/pfm/MainActivity.kt
+++ app/src/main/java/com/aerodeko/pfm/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
@@ -21,6 +22,9 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        // FIXME: remove this shit
+        Log.d("test", "test")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
