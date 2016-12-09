899fbb08e66965c8b374cde99f13f83a6f7b8534
Johan Halin <johan@richie.fi>
2016-12-09 16:02:01 +0200

    Maybe don’t check for annotations.
    I doubt they’re particularly necessary when using Kotlin.

--- ci/update_android_dependencies.sh
+++ ci/update_android_dependencies.sh
@@ -13,8 +13,8 @@ if [ ! -d "/usr/local/android-sdk-linux/build-tools/24.0.3" ]; then
     echo y | android update sdk --no-ui --all --filter "build-tools-24.0.3"
 fi
 
-SUPPORT_VERSION=`grep "com.android.support:support-annotations:" $BUILD_GRADLE | sed "s/.*support-annotations:\(.*\)'.*/\1/"`
-if [ ! -d "/usr/local/android-sdk-linux/extras/android/m2repository/com/android/support/support-annotations/$SUPPORT_VERSION" ]; then
+SUPPORT_VERSION=`grep "com.android.support:appcompat-v7:" $BUILD_GRADLE | sed "s/.*appcompat-v7:\(.*\)'.*/\1/"`
+if [ ! -d "/usr/local/android-sdk-linux/extras/android/m2repository/com/android/support/appcompat-v7/$SUPPORT_VERSION" ]; then
     echo y | android update sdk --no-ui --all --filter "extra-android-m2repository"
 fi
 
