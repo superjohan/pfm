d32e9bda86ccd8fc1e38d1a38576039fa360c69a
Johan Halin <johan@halin.me>
2016-12-11 14:10:38 +0200

    More Calendar extensions.
    Setter for the time in days (for testing), and constructor function for creating a Calendar instance from days (for creation from the database).

--- app/src/main/java/com/aerodeko/pfm/extensions/Calendar.kt
+++ app/src/main/java/com/aerodeko/pfm/extensions/Calendar.kt
@@ -7,5 +7,16 @@ import java.util.concurrent.TimeUnit
  * Created by rm on 11/12/2016.
  */
 
-val Calendar.timeInDays: Long
+var Calendar.timeInDays: Long
     get() = TimeUnit.MILLISECONDS.toDays(this.timeInMillis)
+
+    set(value) {
+        this.timeInMillis = TimeUnit.DAYS.toMillis(value)
+    }
+
+fun calendarFromDays(days: Long): Calendar {
+    val date = Calendar.getInstance()
+    date.timeInDays = days
+
+    return date
+}
