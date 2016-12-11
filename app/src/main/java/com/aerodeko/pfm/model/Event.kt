a759ae5f59d50eca8a94da05acb24ebd1fe4dbf2
Johan Halin <johan@halin.me>
2016-12-11 12:46:39 +0200

    The description is optional.
    Probably won’t even be supported initially. I don’t care about descriptions.

--- app/src/main/java/com/aerodeko/pfm/model/Event.kt
+++ app/src/main/java/com/aerodeko/pfm/model/Event.kt
@@ -6,4 +6,4 @@ import java.util.*
  * Created by rm on 10/12/2016.
  */
 
-data class Event(val date: Date, val value: Double, var description: String)
+data class Event(val date: Date, val value: Double, var description: String?)
