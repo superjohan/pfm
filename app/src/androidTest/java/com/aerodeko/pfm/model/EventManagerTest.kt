85708de752cfae7145a3222d1c6a1ad85b777702
Johan Halin <johan@halin.me>
2016-12-11 17:59:15 +0200

    Test verifying that getting events for a certain date works.

--- app/src/androidTest/java/com/aerodeko/pfm/model/EventManagerTest.kt
+++ app/src/androidTest/java/com/aerodeko/pfm/model/EventManagerTest.kt
@@ -37,4 +37,23 @@ class EventManagerTest {
 
         assertEquals("events should be equal", event, eventFromDatabase)
     }
+
+    @Test
+    fun test_getting_event_for_a_certain_date_only_gets_events_for_that_date() {
+        val date = Calendar.getInstance()
+        val otherDate = date.clone() as Calendar
+        otherDate.add(Calendar.DAY_OF_MONTH, 1)
+
+        val event1_date1 = eventManager.addEvent(1.0, date.time)
+        val event1_date2 = eventManager.addEvent(3.0, date.time)
+        val event2 = eventManager.addEvent(2.0, otherDate.time)
+        val events1 = eventManager.getEvents(date.time)
+        val events2 = eventManager.getEvents(otherDate.time)
+
+        assertEquals("only two events for date", 2, events1.size)
+        assertEquals("only one event for date", 1, events2.size)
+        assertEquals("events for the date should be equal", event1_date1, events1[0])
+        assertEquals("events for the date should be equal", event1_date2, events1[1])
+        assertEquals("events for the date should be equal", event2, events2[0])
+    }
 }
