8e75a84d24e4e1034238b7684b0e0ab759bb0308
Johan Halin <johan@halin.me>
2016-12-11 13:11:33 +0200

    Using Calendar makes more sense.
    It has a nicer API, too.

--- app/src/main/java/com/aerodeko/pfm/model/EventManager.kt
+++ app/src/main/java/com/aerodeko/pfm/model/EventManager.kt
@@ -3,7 +3,6 @@ package com.aerodeko.pfm.model
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import com.aerodeko.pfm.extensions.dateAsSeconds
 import java.util.*
 
 /**
@@ -18,9 +17,9 @@ class EventManager(context: Context) {
         database = databaseHelper.writableDatabase
     }
 
-    fun addEvent(value: Double, date: Date = Date(), description: String? = null): Event? {
+    fun addEvent(value: Double, date: Calendar = Calendar.getInstance(), description: String? = null): Event? {
         val values = ContentValues()
-        values.put(EventDatabaseHelper.Event.Columns.DATE, date.dateAsSeconds)
+        values.put(EventDatabaseHelper.Event.Columns.DATE, date.timeInMillis)
         values.put(EventDatabaseHelper.Event.Columns.VALUE, value)
         values.put(EventDatabaseHelper.Event.Columns.DESCRIPTION, description)
 
