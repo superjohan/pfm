576b8dd5dfc3b375d053e3803d16679f7c944043
Johan Halin <johan@halin.me>
2016-12-11 14:13:11 +0200

    Basic test class for EventManager.
    Has to be run on the device, due to SQLiteOpenHelper etc.

---
+++ app/src/androidTest/java/com/aerodeko/pfm/model/EventManagerTest.kt
@@ -0,0 +1,42 @@
+package com.aerodeko.pfm.model
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.*
+
+/**
+ * Created by rm on 11/12/2016.
+ */
+@RunWith(AndroidJUnit4::class)
+class EventManagerTest {
+    private lateinit var eventManager: EventManager
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getTargetContext()
+
+        eventManager = EventManager(context)
+        eventManager.clearEvents()
+    }
+
+    @Test
+    fun test_adding_event_creates_valid_event() {
+        val expectedValue = 1.0
+
+        val date = Calendar.getInstance()
+        date.timeInMillis = 1234567890
+
+        val event = eventManager.addEvent(expectedValue, date)
+        val events = eventManager.getEvents(date)
+
+        assertEquals("only one event should be received", 1, events.size)
+
+        val eventFromDatabase = events[0]
+
+        assertEquals("events should be equal", event, eventFromDatabase)
+    }
+}
