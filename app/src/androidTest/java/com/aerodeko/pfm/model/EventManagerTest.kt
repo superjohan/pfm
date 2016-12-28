c88aa4f58b1f86bbb3a6826cef0d958b0e2804d3
Johan Halin <johan@halin.me>
2016-12-28 20:13:05 +0200

    Adjusted tests to use budgets instead of pure dates.

--- app/src/androidTest/java/com/aerodeko/pfm/model/EventManagerTest.kt
+++ app/src/androidTest/java/com/aerodeko/pfm/model/EventManagerTest.kt
@@ -26,10 +26,13 @@ class EventManagerTest {
     @Test
     fun test_adding_event_creates_valid_event() {
         val expectedValue = 1.0
+        // this bit with the dates sucks. Do Better
         val date = Date(1234567890)
+        val date2 = Date(2000000000)
+        val budget = Budget("budget", date, date2)
 
         val event = eventManager.addEvent(expectedValue, date)
-        val events = eventManager.getEvents(date)
+        val events = eventManager.getEvents(budget)
 
         assertEquals("only one event should be received", 1, events.size)
 
@@ -40,15 +43,21 @@ class EventManagerTest {
 
     @Test
     fun test_getting_event_for_a_certain_date_only_gets_events_for_that_date() {
-        val date = Calendar.getInstance()
-        val otherDate = date.clone() as Calendar
-        otherDate.add(Calendar.DAY_OF_MONTH, 1)
+        val date1 = Calendar.getInstance()
+        val date2 = date1.clone() as Calendar
+        date2.add(Calendar.DAY_OF_MONTH, 1)
+        val date3 = date2.clone() as Calendar
+        date3.add(Calendar.DAY_OF_MONTH, 1)
 
-        val event1_date1 = eventManager.addEvent(1.0, date.time)
-        val event1_date2 = eventManager.addEvent(3.0, date.time)
-        val event2 = eventManager.addEvent(2.0, otherDate.time)
-        val events1 = eventManager.getEvents(date.time)
-        val events2 = eventManager.getEvents(otherDate.time)
+        val event1_date1 = eventManager.addEvent(1.0, date1.time)
+        val event1_date2 = eventManager.addEvent(3.0, date1.time)
+        val event2 = eventManager.addEvent(2.0, date2.time)
+
+        val budget1 = Budget("budget 1", date1.time, date2.time)
+        val budget2 = Budget("budget 2", date2.time, date3.time)
+
+        val events1 = eventManager.getEvents(budget1)
+        val events2 = eventManager.getEvents(budget2)
 
         assertEquals("only two events for date", 2, events1.size)
         assertEquals("only one event for date", 1, events2.size)
