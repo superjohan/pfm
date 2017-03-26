d11c92f73b65a9c98695fa22d96fdfef401ec2b0
Johan Halin <johan@halin.me>
2017-03-26 17:05:04 +0300

    We can just parse the build tools version from build.gradle.

--- ci/update_android_dependencies.sh
+++ ci/update_android_dependencies.sh
@@ -9,8 +9,9 @@ if [ ! -d "/usr/local/android-sdk-linux/platforms/android-$SDK_VERSION" ]; then
     echo y | android update sdk --no-ui --all --filter "android-$SDK_VERSION"
 fi
 
-if [ ! -d "/usr/local/android-sdk-linux/build-tools/24.0.3" ]; then
-    echo y | android update sdk --no-ui --all --filter "build-tools-24.0.3"
+BUILD_TOOLS_VERSION=`grep "buildToolsVersion" $BUILD_GRADLE | sed "s/.*buildToolsVersion \"\(.*\)\"/\1/"`
+if [ ! -d "/usr/local/android-sdk-linux/build-tools/$BUILD_TOOLS_VERSION" ]; then
+    echo y | android update sdk --no-ui --all --filter "build-tools-$BUILD_TOOLS_VERSION"
 fi
 
 SUPPORT_VERSION=`grep "com.android.support:appcompat-v7:" $BUILD_GRADLE | sed "s/.*appcompat-v7:\(.*\)'.*/\1/"`
