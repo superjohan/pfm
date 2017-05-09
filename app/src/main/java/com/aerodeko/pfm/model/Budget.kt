017bc07e8da6e6aeff97bf81e43e81f6fced7898
Johan Halin <johan@halin.me>
2017-05-09 21:10:33 +0300

    Added TODOs.

--- app/src/main/java/com/aerodeko/pfm/model/Budget.kt
+++ app/src/main/java/com/aerodeko/pfm/model/Budget.kt
@@ -6,4 +6,5 @@ import java.util.*
  * Created by rm on 28/12/2016.
  */
 
+// TODO: amount should be Long
 data class Budget(val name: String, val amount: Double, val startDate: Date, val endDate: Date)
--- app/src/main/java/com/aerodeko/pfm/model/Event.kt
+++ app/src/main/java/com/aerodeko/pfm/model/Event.kt
@@ -6,4 +6,5 @@ import java.util.*
  * Created by rm on 10/12/2016.
  */
 
+// TODO: Value should be Long
 data class Event(val date: Date, val value: Double, var description: String?)
