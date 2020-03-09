7eaad26a5722281f8276674570224c0725edea97
Johan Halin <johan@halin.me>
2020-03-09 20:49:16 +0200

    Migrate to AndroidX.

--- app/src/main/java/com/aerodeko/pfm/MainActivity.kt
+++ app/src/main/java/com/aerodeko/pfm/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.aerodeko.pfm
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import com.aerodeko.pfm.model.BudgetInteractor
